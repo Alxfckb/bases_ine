443f4b60686bde433ea8523d128045b2ef04445a
Alexander Alvarado <daniel.alvarado004@gmail.com>
2023-02-13 22:37:20 -0600

    xml files done

--- xml/xml_2_xl.py
+++ xml/xml_2_xl.py
@@ -11,7 +11,7 @@ from os.path import isfile, isdir, join
 OBTENER LA ENTRADA Y SALIDA DEL USUARIO
 
 '''
-'''
+
 parser = argparse.ArgumentParser(description='***** XML TO EXCEL *****')
 
 parser.add_argument('--user', metavar='-usuario', type=str,
@@ -28,12 +28,32 @@ path = args.user
 outname = args.output+'.xlsx'
 
 cols = ['uuid', 'start', 'end', 'username', 'deviceid', 'ID_Region', 'ID_Departamento', 'Municipio', 'Decada', 'Usuario', 'NumeroBoleta', 'TipoFuente', 'CodigoFuente', 'NombreFuente', 'Direccion', 'Zona', 'Telefono', 'Correo', 'NombreInformante', 'CargoInformante', 'Observacion', 'NumerodeArticulos', 'AgregarFoto', 'Foto', 'GPS', 'FechaRecopilacion', '__version__', '_version_', '_version__001', '_version__002', '_version__003', '_version__004', '_version__005', 'instanceID']
-''' 
 
-path = "example.xml"
+#path = "example.xml"
+cols = {'uuid':[] ,'ID_Region':[], 'ID_Departamento':[], 'Municipio':[] , 'Decada':[] , 'Usuario':[] , 'NumeroBoleta':[] ,
+ 'TipoFuente':[] , 'CodigoFuente':[] , 'NombreFuente':[] , 'Direccion':[] , 'Zona':[] , 'Telefono':[] ,
+  'Correo':[] , 'NombreInformante':[] , 'CargoInformante':[] , 'Observacion':[] , 'NumerodeArticulos':[] ,
+   'AgregarFoto':[] , 'Foto':[] , 'GPS':[] , 'FechaRecopilacion':[], 'instanceID':[] }
+col = ['uuid', 'ID_Region', 'ID_Departamento', 'Municipio', 'Decada', 'Usuario', 'NumeroBoleta', 'TipoFuente', 'CodigoFuente', 'NombreFuente', 'Direccion', 'Zona', 'Telefono', 'Correo', 'NombreInformante', 'CargoInformante', 'Observacion', 'NumerodeArticulos', 'AgregarFoto', 'Foto', 'GPS', 'FechaRecopilacion', 'instanceID']
 
-tree = ET.parse(path)
-root = tree.getroot()
-for child in root:
-    for grandchild in child:
-        print (grandchild.tag,grandchild[0])
+dataframe = pd.DataFrame(cols)
+
+dirs = [f for f in listdir(path) if isdir(join(path, f))]
+#print (dirs)
+#dataframe = pd.DataFrame()
+#dataframe = dataframe.reindex(columns=cols)  
+for dir in dirs:
+    path = args.user+"/"+dir
+    for filename in listdir(path):
+        if not filename.endswith('.xml'): continue
+        fullname = join(path, filename)
+        tree = ET.parse(fullname)
+        root = tree.getroot()
+        for child in root:
+            #print(child.tag, child.text)
+            for grandchild in child:
+                cols[grandchild.tag].append(grandchild.text)
+#print(cols)
+dataframe = pd.DataFrame.from_dict(cols,orient='index').T
+print(dataframe.info)
+dataframe.to_excel(outname, columns=cols, index=False)
